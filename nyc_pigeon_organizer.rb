10e073daab43d90216b8897821b1a965da6563e3
Samuel Zandi <samuelczandi@gmail.com>
2020-01-03 06:13:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+organized_pigeons = {}
+  data.each do |key, value|
+    value.each do |next_key, names|
+      names.each do |name|
+        
+        if !organized_pigeons[name]
+          organized_pigeons[name] = {}
+        end
+
+        if !organized_pigeons[name][key]
+          organized_pigeons[name][key] = []
+        end
+        
+        organized_pigeons[name][key] << next_key.to_s
+      end
+    end
+  end
+  organized_pigeons
 end
